d1036ef29bea48faf5eee6b2b8705dda0ce9b045
Marco André <up202004891@edu.fe.up.pt>
2023-10-17 14:47:21 +0100

    Fix

--- tp2/MyGuiInterface.js
+++ tp2/MyGuiInterface.js
@@ -1,35 +1,33 @@
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-import { MyApp } from './MyApp.js';
-import { MyContents } from './MyContents.js';
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+import { MyApp } from "./MyApp.js";
+import { MyContents } from "./MyContents.js";
 
 /**
     This class customizes the gui interface for the app
 */
-class MyGuiInterface  {
+class MyGuiInterface {
+  /**
+   *
+   * @param {MyApp} app The application object
+   */
+  constructor(app) {
+    this.app = app;
+    this.datgui = new GUI();
+    this.contents = null;
+  }
 
-    /**
-     * 
-     * @param {MyApp} app The application object 
-     */
-    constructor(app) {
-        this.app = app
-        this.datgui =  new GUI();
-        this.contents = null
-    }
+  /**
+   * Set the contents object
+   * @param {MyContents} contents the contents objects
+   */
+  setContents(contents) {
+    this.contents = contents;
+  }
 
-    /**
-     * Set the contents object
-     * @param {MyContents} contents the contents objects 
-     */
-    setContents(contents) {
-        this.contents = contents
-    }
-
-    /**
-     * Initialize the gui interface
-     */
-    init() {
-    }
+  /**
+   * Initialize the gui interface
+   */
+  init() {}
 }
 
 export { MyGuiInterface };
