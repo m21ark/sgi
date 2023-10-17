3918255e3e174297cbd659f54824ec3d96cdfbe1
Marco André <up202004891@edu.fe.up.pt>
2023-10-17 14:40:56 +0100

    Setup tp2

--- tp2/MyAxis.js
+++ tp2/MyAxis.js
@@ -1,30 +1,31 @@
-import * as THREE from "three";
-import { MyApp } from "./MyApp.js";
+import * as THREE from 'three';
+import { MyApp } from './MyApp.js';
 
 /**
  * This class contains a 3D axis representation
  */
 class MyAxis extends THREE.Object3D {
-  /**
-   *
-   * @param {MyApp} app the application object
-   * @param {number} size the size of each axis
-   * @param {number} baseRadius the base radius of each axis
-   * @param {number} xxColor the hexadecimal representation of the xx axis color
-   * @param {number} yyColor the hexadecimal representation of the xx axis color
-   * @param {number} zzColor the hexadecimal representation of the zz axis color
-   */
-  constructor(app, size, baseRadius, xxColor, yyColor, zzColor) {
-    super();
-    this.app = app;
-    this.type = "Group";
-    this.size = size || 2;
-    this.baseRadius = baseRadius || 0.05;
-    this.xxColor = xxColor || 0xff0000;
-    this.yyColor = yyColor || 0x00ff00;
-    this.zzColor = zzColor || 0x0000ff;
 
-    /*         // a cone geometry for the xx axis
+    /**
+     * 
+     * @param {MyApp} app the application object
+     * @param {number} size the size of each axis 
+     * @param {number} baseRadius the base radius of each axis
+     * @param {number} xxColor the hexadecimal representation of the xx axis color
+     * @param {number} yyColor the hexadecimal representation of the xx axis color
+     * @param {number} zzColor the hexadecimal representation of the zz axis color
+     */
+    constructor(app, size, baseRadius, xxColor, yyColor, zzColor) {
+        super();
+        this.app = app;
+        this.type = 'Group';
+        this.size = size || 2;
+        this.baseRadius = baseRadius || 0.05;
+        this.xxColor = xxColor || 0xff0000
+        this.yyColor = yyColor || 0x00ff00
+        this.zzColor = zzColor || 0x0000ff
+
+        // a cone geometry for the xx axis
         const xx = new THREE.ConeGeometry( this.baseRadius, this.size, 32 ); 
         const xxMaterial = new THREE.MeshBasicMaterial( {color: this.xxColor} );
         const xxMesh = new THREE.Mesh(xx, xxMaterial ); 
@@ -45,17 +46,13 @@ class MyAxis extends THREE.Object3D {
         const zzMesh = new THREE.Mesh(zz, zzMaterial ); 
         zzMesh.position.set(0,0,this.size/2);
         zzMesh.rotation.x = Math.PI / 2;
-        this.add( zzMesh );  */
+        this.add( zzMesh ); 
 
-    // an axis helper
-    const axesHelper = new THREE.AxesHelper(5);
-    axesHelper.setColors(
-      new THREE.Color(this.xxColor),
-      new THREE.Color(this.yyColor),
-      new THREE.Color(this.zzColor)
-    );
-    this.add(axesHelper);
-  }
+        // an axis helper
+        const axesHelper = new THREE.AxesHelper( 5 );
+        axesHelper.setColors ( new THREE.Color( this.xxColor ),  new THREE.Color( this.yyColor ),  new THREE.Color( this.zzColor ))
+        this.add( axesHelper );
+    }
 }
 
 MyAxis.prototype.isGroup = true;
--- tp2/MyContents.js
+++ tp2/MyContents.js
@@ -1,198 +1,94 @@
-import * as THREE from "three";
-import { MyAxis } from "./MyAxis.js";
-import { Cake } from "./objects/cake.js";
-import { Table } from "./objects/table.js";
-import { Chair } from "./objects/chair.js";
-
+import * as THREE from 'three';
+import { MyAxis } from './MyAxis.js';
+import { MyFileReader } from './parser/MyFileReader.js';
 /**
  *  This class contains the contents of out application
  */
-class MyContents {
-  constructor(app) {
-    this.app = app;
-    this.axis = null;
+class MyContents  {
 
-    // box related attributes
-    this.boxMesh = null;
-    this.boxMeshSize = 1.0;
-    this.boxEnabled = true;
-    this.lastBoxEnabled = null;
-    this.boxDisplacement = new THREE.Vector3(0, 2, 0);
-  }
+    /**
+       constructs the object
+       @param {MyApp} app The application object
+    */ 
+    constructor(app) {
+        this.app = app
+        this.axis = null
 
-  // ============== Materials ====================
-
-  floorMaterial = new THREE.MeshPhongMaterial({
-    color: "#00ffff",
-    specular: "#777777",
-    emissive: "#000000",
-    shininess: 30,
-  });
-
-  wallMaterial = new THREE.MeshPhongMaterial({
-    color: "#cc9900",
-    specular: "#ccbb00",
-    emissive: "#000000",
-    shininess: 30,
-  });
-
-  tableWoodMaterial = new THREE.MeshPhongMaterial({
-    color: "#8B4513",
-    specular: "#8B4513",
-    emissive: "#000000",
-    shininess: 30,
-  });
-
-  silverMaterial = new THREE.MeshPhongMaterial({
-    color: "#c0c0c0",
-    specular: "#ffffff",
-    emissive: "#000000",
-    shininess: 30,
-  });
-
-  chocolateMaterial = new THREE.MeshPhongMaterial({
-    color: "#5C4033",
-    specular: "#222222",
-    emissive: "#000000",
-    shininess: 30,
-    side: THREE.DoubleSide, // Render both sides of the faces
-  });
-
-  carpetMaterial = new THREE.MeshPhongMaterial({
-    color: "#aa00ff",
-    specular: "#cc00ff",
-    emissive: "#000000",
-    shininess: 30,
-  });
-
-  tvMaterial = new THREE.MeshPhongMaterial({
-    color: "#222222",
-    specular: "#353535",
-    emissive: "#000000",
-    shininess: 30,
-  });
-
-  // ============== Objects ====================
-
-  table = new Table(6, 0.2, 10, this.tableWoodMaterial, 2.0);
-  floor = new THREE.BoxGeometry(15, 0.1, 15);
-  wall = new THREE.BoxGeometry(15, 5, 0.1);
-  dish = new THREE.CylinderGeometry(1.3, 1, 0.25, 32);
-  chairs = [
-    new Chair(2.5, 0.2, 2.5, this.tableWoodMaterial, [2.5, 2.8, 1]),
-    new Chair(2.5, 0.2, 2.5, this.tableWoodMaterial, [-2.5, 2.8, -1]),
-    new Chair(2.5, 0.2, 2.5, this.tableWoodMaterial, [2.5, -2.8, 1]),
-    new Chair(2.5, 0.2, 2.5, this.tableWoodMaterial, [-2.5, -2.8, -1]),
-  ];
-
-  carpet = new THREE.PlaneGeometry(12, 8, 32);
-
-  cake = new Cake(
-    4,
-    3.2,
-    16,
-    2,
-    false,
-    Math.PI * 0.25,
-    Math.PI * 1.54,
-    this.chocolateMaterial
-  );
-
-  tv = new THREE.PlaneGeometry(8, 3.5, 32);
-
-  // ============== Meshes ====================
-
-  floorMesh = new THREE.Mesh(this.floor, this.floorMaterial);
-
-  // walls
-  wallMesh1 = new THREE.Mesh(this.wall, this.wallMaterial);
-  wallMesh2 = new THREE.Mesh(this.wall, this.wallMaterial);
-  wallMesh3 = new THREE.Mesh(this.wall, this.wallMaterial);
-  wallMesh4 = new THREE.Mesh(this.wall, this.wallMaterial);
-
-  // dish with cake
-  dishMesh = new THREE.Mesh(this.dish, this.silverMaterial);
-
-  // carpet
-  carpetMesh = new THREE.Mesh(this.carpet, this.carpetMaterial);
-
-  // tv
-  tvMesh = new THREE.Mesh(this.tv, this.tvMaterial);
-
-  init() {
-    this.axis = new MyAxis(this);
-    // this.app.scene.add(this.axis);
-
-    // ============== Positions ====================
-
-    this.floorMesh.position.y = -0.05;
-
-    this.wallMesh1.position.y = 2.5;
-    this.wallMesh1.position.z = 7.5;
-
-    this.wallMesh2.position.y = 2.5;
-    this.wallMesh2.position.z = -7.5;
-
-    this.wallMesh3.position.y = 2.5;
-    this.wallMesh3.position.x = -7.5;
-    this.wallMesh3.rotation.y = Math.PI / 2;
-
-    this.wallMesh4.position.y = 2.5;
-    this.wallMesh4.position.x = 7.5;
-    this.wallMesh4.rotation.y = Math.PI / 2;
-
-    this.dishMesh.position.y = 2.2;
-    this.dishMesh.position.x = 0;
-
-    this.cake.position.y = 2.6;
-    this.cake.position.x = 0;
-    this.cake.scale.set(0.2, 0.3, 0.2);
-
-    this.carpetMesh.position.y = 0.02;
-    this.carpetMesh.rotation.x = -Math.PI / 2;
-
-    this.tvMesh.position.y = 2.6;
-    this.tvMesh.position.z = -7.44;
-
-    // ============== Display ====================
-
-    this.app.scene.add(this.table);
-    this.app.scene.add(this.floorMesh);
-    this.app.scene.add(this.wallMesh1);
-    this.app.scene.add(this.wallMesh2);
-    this.app.scene.add(this.wallMesh3);
-    // this.app.scene.add(this.wallMesh4); // desligar para ver a mesa
-
-    for (let chair in this.chairs) {
-      this.app.scene.add(this.chairs[chair]);
+        this.reader = new MyFileReader(app, this, this.onSceneLoaded);
+		this.reader.open("scenes/demo/demo.xml");		
     }
 
-    this.app.scene.add(this.dishMesh);
-    this.app.scene.add(this.cake);
+    /**
+     * initializes the contents
+     */
+    init() {
+        // create once 
+        if (this.axis === null) {
+            // create and attach the axis to the scene
+            this.axis = new MyAxis(this)
+            this.app.scene.add(this.axis)
+        }
+    }
 
-    this.app.scene.add(this.carpetMesh);
-    this.app.scene.add(this.tvMesh);
+    /**
+     * Called when the scene xml file load is complete
+     * @param {MySceneData} data the entire scene data object
+     */
+    onSceneLoaded(data) {
+        console.info("scene data loaded " + data + ". visit MySceneData javascript class to check contents for each data item.")
+        this.onAfterSceneLoadedAndBeforeRender(data);
+    }
 
-    // ================== Lights ====================
+    output(obj, indent = 0) {
+        console.log("" + new Array(indent * 4).join(' ') + " - " + obj.type + " " + (obj.id !== undefined ? "'" + obj.id + "'" : ""))
+    }
 
-    // add a point light on top of the model
-    const pointLight = new THREE.PointLight(0xffffff, 250, 0);
-    pointLight.position.set(0, 10, 0);
-    this.app.scene.add(pointLight);
+    onAfterSceneLoadedAndBeforeRender(data) {
+       
+        // refer to descriptors in class MySceneData.js
+        // to see the data structure for each item
 
-    // add a point light helper for the previous point light
-    const sphereSize = 0.5;
-    const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
-    this.app.scene.add(pointLightHelper);
+        this.output(data.options)
+        console.log("textures:")
+        for (var key in data.textures) {
+            let texture = data.textures[key]
+            this.output(texture, 1)
+        }
 
-    // add an ambient light
-    const ambientLight = new THREE.AmbientLight(0x555555);
-    this.app.scene.add(ambientLight);
-  }
+        console.log("materials:")
+        for (var key in data.materials) {
+            let material = data.materials[key]
+            this.output(material, 1)
+        }
 
-  update() {
-    // ...
-  }
+        console.log("cameras:")
+        for (var key in data.cameras) {
+            let camera = data.cameras[key]
+            this.output(camera, 1)
+        }
+
+        console.log("nodes:")
+        for (var key in data.nodes) {
+            let node = data.nodes[key]
+            this.output(node, 1)
+            for (let i=0; i< node.children.length; i++) {
+                let child = node.children[i]
+                if (child.type === "primitive") {
+                    console.log("" + new Array(2 * 4).join(' ') + " - " + child.type + " with "  + child.representations.length + " " + child.subtype + " representation(s)")
+                    if (child.subtype === "nurbs") {
+                        console.log("" + new Array(3 * 4).join(' ') + " - " + child.representations[0].controlpoints.length + " control points")
+                    }
+                }
+                else {
+                    this.output(child, 2)
+                }
+            }
+        }
+    }
+
+    update() {
+        
+    }
 }
 
 export { MyContents };
