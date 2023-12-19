c2ebdc5b27ca0ea47b71728135f102e697937067
Marco André <marcoandre2103@gmail.com>
2023-12-19 12:36:23 +0000

    Fixed bug with picker

--- tp3/MyPicker.js
+++ tp3/MyPicker.js
@@ -122,8 +122,6 @@ export class MyPicker {
     //3. compute intersections
     let intersects = this.raycaster.intersectObjects(this.app.scene.children);
 
-    console.log("Clicked ", this.raycaster);
-
     // indicate the object name that is being picked
     if (intersects.length > 0) {
       const obj = intersects[0].object;
