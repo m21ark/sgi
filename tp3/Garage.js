4ca5840469bd381d45f34e067f7d0318c1c77034
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-12-19 17:23:47 +0000

    stop animation

--- tp3/Garage.js
+++ tp3/Garage.js
@@ -3,9 +3,6 @@ import * as THREE from "three";
 
 export class Garage {
 
-
-
-
     static openGarage() {
         // search for the object named "door" in the objectModel
         console.log(Garage.objectModel);
@@ -23,11 +20,43 @@ export class Garage {
         );
         const keyFrameClip = new THREE.AnimationClip("Door", 10, [keyFrameAnim]);
         Garage.mixer = new THREE.AnimationMixer(door);
+        
+        
         const action = Garage.mixer.clipAction(keyFrameClip);
+        // Add an event listener for the end of the animation
+        Garage.mixer.addEventListener('loop', (e) => {
+
+            Garage.mixer.stopAllAction(); // Stop the animation mixer to prevent further updates
+            door.rotateZ(angle);
+            Garage.closeGarage(door);
+        });
 
         action.play();
-
     }
+
+    static closeGarage(door) {
+
+        // close the door using animation rotation
+        const angle = Math.PI / 2; // example angle value
+        const quaternion = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), angle);
+        const keyFrameAnim = new THREE.QuaternionKeyframeTrack(
+            ".quaternion",
+            [0, 10],
+            [ quaternion.x, quaternion.y, quaternion.z, quaternion.w, 0,0,0,0] // pass the quaternion values as keyframes
+        );
+        const keyFrameClip = new THREE.AnimationClip("Door", 10, [keyFrameAnim]);
+        Garage.mixer = new THREE.AnimationMixer(door);
+        const action = Garage.mixer.clipAction(keyFrameClip);
+        // Add an event listener for the end of the animation
+        Garage.mixer.addEventListener('loop', (e) => {
+
+            Garage.mixer.stopAllAction(); // Stop the animation mixer to prevent further updates
+            door.rotateZ(-angle);
+        });
+
+        action.play();
+    }
+
 
     static update() {
         if (this.mixer !== undefined && Garage.clock != undefined) {
