eafbdea5743dfda87629a00da78a3a37f9e8441d
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-12-21 18:40:09 +0000

    Contente marco ?

--- tp3/utils/FirstPersonCamera.js
+++ tp3/utils/FirstPersonCamera.js
@@ -41,9 +41,64 @@ export class FirstPersonCamera {
   normalizeRadian(angle) {
     return (angle % (2 * Math.PI) + 2 * Math.PI) % (2 * Math.PI);
   }
-  
+
+  update2() {
+    const playerSpeed = 0.5;
+    const rotationSpeed = 0.05;
+    const playerDirection = new THREE.Vector3(0, 0, -1); // Initial forward direction
+
+    // Rotate the player's direction based on their current rotation
+    playerDirection.applyAxisAngle(
+      new THREE.Vector3(0, 1, 0),
+      this.player.rotation.y
+    );
+
+    // Calculate the movement vector based on the player's direction
+    const moveVector = new THREE.Vector3();
+    if (this.keyboard["w"]) moveVector.sub(playerDirection);
+    if (this.keyboard["s"]) moveVector.add(playerDirection);
+    if (this.keyboard["a"]) {
+      const leftDirection = new THREE.Vector3(1, 0, 0);
+      leftDirection.applyAxisAngle(
+        new THREE.Vector3(0, 1, 0),
+        this.player.rotation.y
+      );
+      moveVector.add(leftDirection);
+    }
+    if (this.keyboard["d"]) {
+      const rightDirection = new THREE.Vector3(-1, 0, 0);
+      rightDirection.applyAxisAngle(
+        new THREE.Vector3(0, 1, 0),
+        this.player.rotation.y
+      );
+      moveVector.add(rightDirection);
+    }
+
+    // MOVE UP AND DOWN
+    if (this.keyboard[" "]) moveVector.add(new THREE.Vector3(0, 1, 0));
+    if (this.keyboard["shift"]) moveVector.sub(new THREE.Vector3(0, 1, 0));
+
+    // Normalize the move vector and apply playerSpeed
+    moveVector.normalize().multiplyScalar(playerSpeed);
+
+    // Update player position
+    this.player.position.add(moveVector);
+
+    if (this.keyboard["arrowleft"]) this.player.rotation.y += rotationSpeed;
+    if (this.keyboard["arrowright"]) this.player.rotation.y -= rotationSpeed;
+
+    this.updatePlayerCamera();
+
+  }
+
   update() {
     const playerDirection = new THREE.Vector3(0, 0, -1); // Initial forward direction
+
+    // if this.player doesn't have a friction function, then call update2() instead
+    if (!this.player.friction) {
+      this.update2();
+      return;
+    }
 
     // Rotate the player's direction based on their current rotation
     playerDirection.applyAxisAngle(
